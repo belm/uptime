141794a80b572e48c7349e99bb05b7a7e7961693
lumeng <belm@users.noreply.github.com>
2024-10-11 17:04:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -22,15 +22,11 @@ window.Config = {
   Navi: [
     {
       text: '主页',
-      url: 'https://uptime.001315.xyz/'
+      url: 'https://uptime.ttsdk.com/'
     },    
     {
       text: 'GitHub',
-      url: 'https://github.com/shaoyouvip/uptime/'
-    },
-    {
-      text: '博客',
-      url: 'https://blog.001315.xyz/'
-    },
+      url: 'https://github.com/belm/uptime/'
+    }
   ],
 };
